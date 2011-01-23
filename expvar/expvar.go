003c754e10416b952ec7cc83beb5eecbb44bf6b5
Gary Burd <gary@hello9-2.local>
2011-01-22 23:53:14 -0800

    add expvar package

---
+++ expvar/expvar.go
@@ -0,0 +1,170 @@
+// Copyright 2011 Gary Burd
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// The expvar package provides an interface for registering and publishing
+// objects as JSON over HTTP. This is useful for publishing counters and other
+// operational data to monitoring tools.
+// 
+// The application should wrap the ServeWeb function in this package with
+// appropriate access control and register the resulting handler with a web
+// server.
+//
+// This package differs from the standard library's expvar package in two ways:
+// This package does not have a dependency on the standard library's http
+// server. This package relies on JSON marshaling more directly.
+package expvar
+
+import (
+	"json"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+	"github.com/garyburd/twister/web"
+)
+
+var (
+	mutex sync.Mutex
+	vars  = map[string]interface{}{}
+)
+
+// Publish adds v to the root level JSON object published by this package. The
+// value v is any type that can be encoded by calling json.Marshal. If a name
+// is already registered, then the function will panic. This function is
+// typically called from a package's init() funtion. 
+func Publish(name string, v interface{}) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if _, existing := vars[name]; existing {
+		log.Panicln("Reuse of published var name:", name)
+	}
+	vars[name] = v
+}
+
+// The MarshalJSONFunc type is an adapter to allow the use of ordinary
+// functions as JSON marshallers. The function is called each time the object
+// is marshaled.
+type MarshalJSONFunc func() ([]byte, os.Error)
+
+func (f MarshalJSONFunc) MarshalJSON() ([]byte, os.Error) {
+	return f()
+}
+
+// ValueFunc wraps a func() interface{} with JSON marshalling of the returned
+// value. The function is called each time the object is marshaled.
+type ValueFunc func() interface{}
+
+func (f ValueFunc) MarshalJSON() ([]byte, os.Error) {
+	return json.Marshal(f())
+}
+
+// Map is a synchronized wrapper around a string-to-value map.
+type Map struct {
+	mu sync.Mutex
+	m  map[string]interface{}
+}
+
+// NewMap allocatess a new Map and publishes it using name.
+func NewMap(name string) *Map {
+	m := &Map{}
+	m.Init()
+	Publish(name, m)
+	return m
+}
+
+func (m *Map) MarshalJSON() ([]byte, os.Error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return json.Marshal(m.m)
+}
+
+func (m *Map) Init() *Map {
+	m.m = map[string]interface{}{}
+	return m
+}
+
+func (m *Map) Get(key string) interface{} {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.m[key]
+}
+
+func (m *Map) Set(key string, v interface{}) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.m[key] = v
+}
+
+// AddInt adds delta to the *Int value stored for the given map key.
+func (m *Map) AddInt(key string, delta int64) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	v, ok := m.m[key]
+	if !ok {
+		v = &Int{}
+		m.m[key] = v
+	}
+
+	if iv, ok := v.(*Int); ok {
+		iv.Add(delta)
+	}
+}
+
+// Int is a sychronized wrapper around a 64-bit integer. The Int type is useful
+// implementing counters that can be updated by by concurrent goroutines.
+type Int struct {
+	i  int64
+	mu sync.Mutex
+}
+
+func (i *Int) MarshalJSON() ([]byte, os.Error) {
+	return []byte(strconv.Itoa64(i.i)), nil
+}
+
+// Add atomically adds delta to the counter.
+func (i *Int) Add(delta int64) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.i += delta
+}
+
+// Set atomicaly sets the counter to value.
+func (i *Int) Set(value int64) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.i = value
+}
+
+// NewInt allocatess a new counter and publishes it using name.
+func NewInt(name string) *Int {
+	v := &Int{}
+	Publish(name, v)
+	return v
+}
+
+func ServeWeb(req *web.Request) {
+	b, err := json.MarshalIndent(vars, "", " ")
+	if err != nil {
+		req.Error(web.StatusInternalServerError, err)
+		return
+	}
+	req.Respond(web.StatusOK, web.HeaderContentType, "application/json; charset=utf-8").Write(b)
+}
+
+func init() {
+	start := time.Seconds()
+	Publish("uptimeSeconds", ValueFunc(func() interface{} { return time.Seconds() - start }))
+	Publish("cmdline", os.Args)
+}
--- web/handlers.go
+++ web/handlers.go
@@ -21,8 +21,6 @@ import (
 	"path"
 	"strconv"
 	"strings"
-	"fmt"
-	"expvar"
 )
 
 func serveFile(req *Request, fname string, extraHeader []string) {
@@ -157,22 +155,4 @@ var notFoundHandler = HandlerFunc(func(req *Request) { req.Error(StatusNotFound,
 // NotFoundHandler returns a request handler that responds with 404 not found.
 func NotFoundHandler() Handler {
 	return notFoundHandler
-}
-
-// ExpvarHandler returns a handler that responds with the JSON encoding of
-// variables exported with the expvar package.
-func ExpvarHandler() Handler {
-	return HandlerFunc(func(req *Request) {
-		w := req.Respond(StatusOK, HeaderContentType, "application/json; charset=utf-8")
-		fmt.Fprintf(w, "{\n")
-		first := true
-		for kv := range expvar.Iter() {
-			if !first {
-				fmt.Fprintf(w, ",\n")
-			}
-			first = false
-			fmt.Fprintf(w, "%q: %s", kv.Key, kv.Value)
-		}
-		fmt.Fprintf(w, "\n}\n")
-	})
 }
