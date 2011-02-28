b7afafa5a8e42e1d4a5574ad36dea410aef1ae9a
Gary Burd <gary@beagledreams.com>
2011-02-28 13:09:04 -0800

    Link pkg doc to source on github.

--- pkg/build.sh
+++ pkg/build.sh
@@ -1,4 +1,4 @@
 for i in web server oauth expvar websocket
 do
-    pkgdoc template.html github.com/garyburd/twister/$i > $i.html
+    pkgdoc -basePath=http://github.com/garyburd/twister/blob/`(cd $GOROOT/src/pkg/github.com/garyburd/twister/; git rev-parse HEAD)`/$i/ template.html github.com/garyburd/twister/$i > $i.html
 done
